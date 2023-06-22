683d7567706d2f64ea0f2a2ad31700ffba7bc132
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-06-22 12:37:48 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    screens: { // px
+      'h4': '400px',
+      'h6': '600px',
+      'h8': '800px',
+      'k1': '1000px',
+      'k15': '1500px',
+      'k2': '2000px',
+      'k25': '2500px',
+      'k3': '3000px',
+      'k35': '3500px',
+    },
+    extend: {
+      animation: {
+        'wiggle': 'wiggle 0.5s ease-in-out',
+      },
+      colors: {
+        neutral: '#e5e7eb',
+        tileBackground: '#d5fff7',
+      },
+      keyframes: {
+        wiggle: {
+          '0%': { transform: 'rotate(0deg)' },
+          '33%': { transform: 'rotate(-3deg)' },
+          '66%': { transform: 'rotate(3deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+
+        flash: {
+          '0%': { opacity: "0" },
+          '50%': { opacity: "100" },
+          '100%': { opacity: "0" },
+        }
+
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
