6ff537b4c18fe485c044854f24469861468f7be2
me <mr.eckardt@gmail.com>
2023-06-24 16:11:36 +0300

    Update digiq_tLandingView.vue

--- digiq_t/digiq_tLandingView.vue
+++ digiq_t/digiq_tLandingView.vue
@@ -2,7 +2,7 @@
   <div class="landing -mt-32">
     <LSection class="h-screen">
       <template #body>
-        <MenuBar class="fixed w-full z-50 top-0"></MenuBar>
+        <MenuBar class="fixed w-full z-50 left-0 top-0"></MenuBar>
         <div class="flex flex-col pt-20 min-h-screen">
           <div class="flex-grow flex flex-col">
             <router-view class="flex-grow" name="header"></router-view>
